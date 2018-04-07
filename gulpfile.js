f70e8a20a49251a5b3cfeac9c55f9ba516833b9c
Vinicius <vinicius_k2013@hotmail.com>
2018-04-07 18:44:02 -0300

    Pagina inicial

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,47 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+var gulp = require('gulp')
+	,sass = require('gulp-sass')
+	,watch = require('gulp-watch')
+	,clean = require('gulp-clean')
+	,browserSync = require('browser-sync').create();
 
 // task para o sass
 gulp.task('sass', function() {
-    return gulp.src('sass/**/*.scss')
+    return gulp.src('source/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('source/css'));
 });
 
 // task para watch
 gulp.task('watch', function(){
-	gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch('source/sass/**/*.scss', ['sass']);
 });
 
 // task default gulp
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['watch', 'sass', 'copy']);
+
+// Tarefa para copiar Gulp
+gulp.task('copy', ['clean', 'sass', 'server'], function(){
+    gulp.src('source/**/*')
+    .pipe(gulp.dest('deploy'));
+});
+
+//
+gulp.task('clean', function(){
+	return gulp.src('dist')
+	.pipe(clean());
+});
+
+//
+gulp.task('server', function(){
+	browserSync.init({
+		server: {
+			baseDir: 'source'
+		}
+	});
+
+	return gulp.watch('source/**/*').on('change', browserSync.reload);
+
+});
+
+
+
